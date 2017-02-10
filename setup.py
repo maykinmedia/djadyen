2db55160e510f823ad8b8a948515d50be441744f
Jorik Kraaikamp <jorik.kraaikamp@maykinmedia.nl>
2017-02-10 13:48:32 +0100

    updated version to 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='djadyen',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
 
     # packaging
