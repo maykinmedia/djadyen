ed81d412cebfb266d6a8f8c5d20adc0bbfdcd1b5
Jorik Kraaikamp <jorik.kraaikamp@maykinmedia.nl>
2017-02-20 14:15:51 +0100

    updated the version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='djadyen',
-    version='0.1.4',
+    version='0.1.5',
     license='BSD',
 
     # packaging
