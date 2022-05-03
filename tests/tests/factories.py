6d836be2fc5d783263b9cac634b87ffd9256314a
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2022-05-03 16:07:30 +0200

    Update factories.py

--- tests/tests/factories.py
+++ tests/tests/factories.py
@@ -6,12 +6,12 @@ from djadyen.models import AdyenIssuer, AdyenNotification, AdyenPaymentOption
 from ..app.models import Order
 
 
-class NotificationFactory(factory.DjangoModelFactory):
+class NotificationFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = AdyenNotification
 
 
-class PaymentOptionsFactory(factory.DjangoModelFactory):
+class PaymentOptionsFactory(factory.django.DjangoModelFactory):
     name = factory.Faker('name')
     adyen_name = 'ideal'
     guid = 'ee112bdd-b03b-422f-8af2-085c64dd8698'
@@ -20,7 +20,7 @@ class PaymentOptionsFactory(factory.DjangoModelFactory):
         model = AdyenPaymentOption
 
 
-class IssuerFactory(factory.DjangoModelFactory):
+class IssuerFactory(factory.django.DjangoModelFactory):
     payment_option = factory.SubFactory(PaymentOptionsFactory)
     name = factory.Faker('name')
     adyen_id = 1155
@@ -29,7 +29,7 @@ class IssuerFactory(factory.DjangoModelFactory):
         model = AdyenIssuer
 
 
-class OrderFactory(factory.DjangoModelFactory):
+class OrderFactory(factory.django.DjangoModelFactory):
     status = Status.Created
     email = factory.Faker('email')
 
