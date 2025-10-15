77bb0507011f15ca08b2af0811721ec9c2c25bc8
Conor Holden <conor@maykinmedia.nl>
2025-10-15 12:02:20 +0200

    :sparkles: add issuers to the component template tag

--- djadyen/admin.py
+++ djadyen/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-from .models import AdyenIssuer, AdyenNotification, AdyenPaymentOption
+from djadyen.models import AdyenIssuer, AdyenNotification, AdyenPaymentOption
 
 
 @admin.register(AdyenNotification)
@@ -9,10 +10,23 @@ class AdyenNotificationAdmin(admin.ModelAdmin):
     list_filter = ("is_processed", "created_at", "processed_at")
 
 
+class AdyenIssuerInline(admin.TabularInline):
+    model = AdyenIssuer
+    extra = 1
+
+
 @admin.register(AdyenPaymentOption)
 class PaymentOptionAdmin(admin.ModelAdmin):
-    list_display = ("name", "adyen_name", "is_active")
+    list_display = ("name", "adyen_name", "is_active", "show_issuers")
     list_filter = ("is_active",)
+    inlines = [AdyenIssuerInline]
+
+    @admin.display(description=_("Issuers/Brands"), empty_value="---")
+    def show_issuers(self, obj):
+        issuers = obj.adyenissuer_set.all()
+        if issuers:
+            return ", ".join([issuer.adyen_id for issuer in issuers])
+        return None
 
 
 @admin.register(AdyenIssuer)
